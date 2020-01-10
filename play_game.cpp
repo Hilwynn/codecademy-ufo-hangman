6ded79936a20180614dd99dc28ea39095da2e356
Anna Erkers <anna.erkers@willandskill.se>
2020-01-10 10:52:03 +0100

    Create play_game function

---
+++ play_game.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "functions.hpp"
+
+void play_game()
+{
+
+  srand(time(NULL));
+
+  std::string codeword;
+  std::string answer;
+  std::vector<std::string> possible_codewords = {"midsummer", "unnecessary", "practicality", "kaleidoscope", "cinematography", "enigmatic"};
+
+  codeword = possible_codewords[(rand() % possible_codewords.size())];
+
+  for (int i = 0; i < codeword.length(); i++)
+  {
+    answer += "_";
+  }
+
+  greet();
+
+  int misses = 0;
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+
+  while (answer != codeword && misses < 7)
+  {
+
+    display_misses(misses);
+
+    display_status(incorrect, answer);
+
+    std::cout << "\n\nPlease enter your guess: ";
+
+    std::cin >> letter;
+
+    for (int i = 0; i < codeword.length(); i++)
+    {
+
+      if (letter == codeword[i])
+      {
+
+        answer[i] = letter;
+        guess = true;
+      }
+    }
+
+    if (guess)
+    {
+
+      std::cout << "\nCorrect! The word was \"" << codeword << "\".\n";
+    }
+    else
+    {
+
+      std::cout << "\nIncorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+
+    guess = false;
+  }
+
+  end_game(answer, codeword);
+}
